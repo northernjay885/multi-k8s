4ed9135724cf81f5de949b6b60f1e00aeddb55e6
luorenjie <lrj193927@gmail.com>
2021-04-13 19:47:33 -0400

    correct user name

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t northernjay885/multi-client:latest -t northernjay885/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t northernjay885/multi-server -t northernjay885/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t northernjay885/multi-worker -t northernjay885/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push northernjay885/multi-client:latest
-docker push northernjay885/multi-server:latest
-docker push northernjay885/multi-worker:latest
+docker build -t northernjay/multi-client:latest -t northernjay/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t northernjay/multi-server -t northernjay/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t northernjay/multi-worker -t northernjay/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push northernjay/multi-client:latest
+docker push northernjay/multi-server:latest
+docker push northernjay/multi-worker:latest
 
-docker push northernjay885/multi-client:$SHA
-docker push northernjay885/multi-server:$SHA
-docker push northernjay885/multi-worker:$SHA
+docker push northernjay/multi-client:$SHA
+docker push northernjay/multi-server:$SHA
+docker push northernjay/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=northernjay885/multi-server:$SHA
-kubectl set image deployments/client-deployment client=northernjay885/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=northernjay885/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=northernjay/multi-server:$SHA
+kubectl set image deployments/client-deployment client=northernjay/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=northernjay/multi-worker:$SHA
